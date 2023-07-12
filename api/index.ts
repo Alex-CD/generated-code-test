92fa4835b73bc5c06630dd6388f9ad53844e232d
nathanphillips <nphillips@worth.systems>
2023-07-12 14:13:55 +0100

    added swagger

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,67 @@ import fastify from "fastify";
 
 const server = fastify();
 
+server.register(require("@fastify/swagger"), {});
+server.register(require("@fastify/swagger-ui"), {
+  routePrefix: "/docs",
+  swagger: {
+    info: {
+      title: "My FirstAPP Documentation",
+      description: "My FirstApp Backend Documentation description",
+      version: "0.1.0",
+      termsOfService: "https://mywebsite.io/tos",
+      contact: {
+        name: "John Doe",
+        url: "https://www.johndoe.com",
+        email: "john.doe@email.com",
+      },
+    },
+    externalDocs: {
+      url: "https://www.johndoe.com/api/",
+      description: "Find more info here",
+    },
+    host: "127.0.0.1:3000",
+    basePath: "",
+    schemes: ["http", "https"],
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    tags: [
+      {
+        name: "User",
+        description: "User's API",
+      },
+    ],
+    definitions: {
+      User: {
+        type: "object",
+        required: ["id", "email"],
+        properties: {
+          id: {
+            type: "number",
+            format: "uuid",
+          },
+          firstName: {
+            type: "string",
+          },
+          lastName: {
+            type: "string",
+          },
+          email: {
+            type: "string",
+            format: "email",
+          },
+        },
+      },
+    },
+  },
+  uiConfig: {
+    docExpansion: "none", // expand/not all the documentations none|list|full
+    deepLinking: true,
+  },
+  staticCSP: false,
+  exposeRoute: true,
+});
+
 server.get("/ping", async (request, reply) => {
   return "pong\n";
 });
